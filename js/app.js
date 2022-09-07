7b7b6b3ed17323b98a11f1b32952c19e0e15eee9
rodriguez-miguel26 <85662329+rodriguez-miguel26@users.noreply.github.com>
2022-09-06 18:02:07 -0700

    Plug-ins still not working
    js links are not working

--- js/app.js
+++ js/app.js
@@ -1,16 +1,5 @@
-baguetteBox.run('.gallery');
-
-function search() {
-    let input = document.getElementById('searchbox').value
-    input=input.toLowerCase();
-    let x = document.getElementsByClassName('');
-      
-    for (i = 0; i < x.length; i++) { 
-        if (!x[i].innerHTML.toLowerCase().includes(input)) {
-            x[i].style.display="none";
-        }
-        else {
-            x[i].style.display="";                 
-        }
-    }
-}    
+const picseacrh = document.getElementById('picseacrh');
+picseacrh.addEventListener('keyup', e => {
+    let currentValue= e.target.value.toLowerCase();
+    console.log(currentValue);
+});
